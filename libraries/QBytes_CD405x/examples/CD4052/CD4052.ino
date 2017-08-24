7a8f5f4c9eea9999463c7482178903d07a5c3853
Mr. Q <qbytes.dq@gmail.com>
2017-08-23 21:56:33 -0400

    Easier to read CD4052

--- libraries/QBytes_CD405x/examples/CD4052/CD4052.ino
+++ libraries/QBytes_CD405x/examples/CD4052/CD4052.ino
@@ -1,13 +1,7 @@
 #include <CD4052.h>
 
-int addr_A = 4;
-int addr_B = 5;
-
-int adc_X = A6;
-int adc_Y = A7;
-
 //CD4052 cd4052 = CD4052();
-CD4052 cd4052 = CD4052(addr_A, addr_B, adc_X, adc_Y);
+CD4052 cd4052 = CD4052(4, 5, A6, A7);
 
 /*
  * 
@@ -19,6 +13,19 @@ void setup() {
   Serial.println("Hello!, testing CD4052");
 }
 
+
+/*
+ * Print and int variable
+ */
+int printInt(int n, String format){
+  char c[10];        // long enough to hold complete integer string
+  char charBuf[20];
+  format.toCharArray(charBuf,20);
+  int m = sprintf(c, charBuf, n);    // build integer string using C integer formatters  (m is length, and not used in this code)
+  Serial.print(c);   
+  return m;
+}
+
 /*
  * 
  * 
@@ -26,13 +33,13 @@ void setup() {
 void loop() {
       
   // Increment looping
-  for (int looping = 0; looping <= 4; looping ++){
-	  cd4052.setChannel(looping);
-	  Serial.print(cd4052.analogReadX());
-	  Serial.print(" ");
-	  Serial.print(cd4052.analogReadY());
-	  Serial.print(" "); 
+  for (int looping = 0; looping < 4; looping++){
+    cd4052.setChannel(looping);
+    printInt(looping,"<X%1d:");
+    printInt(cd4052.analogReadX(),"%4d> ");
+    printInt(looping,"<Y%1d:");
+    printInt(cd4052.analogReadY(),"%4d> ");
     }
 	
-  Serial.println("=====");
+  Serial.println("");
 }
