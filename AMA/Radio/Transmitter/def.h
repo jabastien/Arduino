75f8c4446c7acb7716e23f677e45fb4249a8a6ed
Mr. Q <qbytes.dq@gmail.com>
2017-11-27 23:49:08 -0500

    Compiiling again

--- AMA/Radio/Transmitter/def.h
+++ AMA/Radio/Transmitter/def.h
@@ -45,7 +45,7 @@ const String err = "Err:
 /**************************************************************************************/
 PROGMEM const char  qBytesWorld[] = "Q-Bytes World";
 PROGMEM const char  deviceInfo [] = {"2.4G Transmitter"};
-PROGMEM const char  versionNum [] = {"v1.17"};
+PROGMEM const char  versionNum [] = {"v1.18"};
 
 /**************************************************************************************/
 /***************          date & time configurations               ********************/
